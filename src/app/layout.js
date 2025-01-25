6559f9a64770c93577c9b0d79ceb7c6f80292c0f
rohhitsingh19 <154086284+rohhitsingh19@users.noreply.github.com>
2025-01-25 12:19:42 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,13 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from './components/header';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,12 +26,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Header/>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
